5b9c4744a3ebf9e3aa4b12eaf1bf3204259b4323
danielmdesigns <danielwmaldonado@gmail.com>
2015-07-25 10:32:45 -0500

    fix dropdown menu when clicked
    • add “use-strict”;
    • fix dropdown menu that only happens when browser is <= 767px
    • fix so that drop down menu will collapse when user select any artist
    link from sub-menu
    • fix dropdown menu so that parent level menu items collapse w/ out
    interfering w/ artist sub menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,64 +1,59 @@
   //Javascript, jQuery, and AJAX
-  /*$(document).ready(function(){
-	  $("#shop").click(function(){
-		  $(".main-container").load("index.html");
-	  });
-  });
-
-  $(document).ready(function(){
-	  $("#faqs").click(function(){
-		  $(".main-container").load("faqs.html");
-	  });
-  });*/
-  
-  
-//this shit worked, hahahha!!!
-//PLAYGROUND - need to add/remove actives 
 $(document).ready(function () {
-    // load index page when the page loads
-    $(".main-container").load("shop.html");
-    $("#shop").click(function () {
-        $(".main-container").load("shop.html");
-    });
-    $("#faqs").click(function () {
-        $(".main-container").load("faqs.html");
-    });
-	$("#richie").click(function () {
-        $(".main-container").load("artist/richie.html");
-    });
-    $("#shay").click(function () {
-        $(".main-container").load("artist/shay.html");
-    });
-    $("#jose").click(function () {
-        $(".main-container").load("artist/jose.html");
-    });
-    $("#marie").click(function () {
-        $(".main-container").load("artist/marie.html");
-    });
-	$("#simon").click(function () {
-        $(".main-container").load("artist/simon.html");
-    });
-	$("#jaimie").click(function () {
-        $(".main-container").load("artist/jaimie.html");
-    });
-	$("#andrew").click(function () {
-        $(".main-container").load("artist/andrew.html");
-    });
-	$("#teddie").click(function () {
-        $(".main-container").load("artist/teddie.html");
-    });
-	
-	//map
-	$("#map").hide();
-  	$("#showmap").on("click", function(){
-    	$("#map").slideToggle("slow");
-		return false;
-  	});
-	
-	//google maps
-	$('address').each(function () {
-	  var link = "<a class='maps' href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
-	  $(this).html(link);
+  "use strict";
+  // load index page when the page loads
+  $(".main-container").load("shop.html");
+  // load page when nav link clicked
+  $("#shop").click(function () {
+	  $(".main-container").load("shop.html");
+  });
+  $("#faqs").click(function () {
+	  $(".main-container").load("faqs.html");
+  });
+  $("#richie").click(function () {
+	  $(".main-container").load("artist/richie.html");
+  });
+  $("#shay").click(function () {
+	  $(".main-container").load("artist/shay.html");
+  });
+  $("#jose").click(function () {
+	  $(".main-container").load("artist/jose.html");
+  });
+  $("#marie").click(function () {
+	  $(".main-container").load("artist/marie.html");
+  });
+  $("#simon").click(function () {
+	  $(".main-container").load("artist/simon.html");
+  });
+  $("#jaimie").click(function () {
+	  $(".main-container").load("artist/jaimie.html");
+  });
+  $("#andrew").click(function () {
+	  $(".main-container").load("artist/andrew.html");
+  });
+  $("#teddie").click(function () {
+	  $(".main-container").load("artist/teddie.html");
+  });
+  
+  if($(window).width() <= 767){
+	$(".dropdown-menu").click(function(){
+	$(".navbar-collapse").collapse('hide');
 	});
-	
+  }
+  if($(window).width() < 767){
+	$("#shop").on("click", function(){
+		$(".navbar-collapse").collapse("hide");
+	});
+  }
+  if($(window).width() < 767){
+	$("#faqs").on("click", function(){
+		$(".navbar-collapse").collapse("hide");
+	});
+  }
+    if($(window).width() < 767){
+	$("#contact").on("click", function(){
+		$(".navbar-collapse").collapse("hide");
+	});
+  }
+  
 });
